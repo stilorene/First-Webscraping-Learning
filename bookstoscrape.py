a8f05e748379ecfaa53586e2828c20656c16f630
Rene Marchner <rene.marchner80@gmail.com>
2024-12-19 23:56:02 +0100

    Oke nochmal ran da, wollte das input in dem Text der liste finden, noch etwas kompliziert aber fast habe ich es geschafft. Heute muss ich pennen, morgen versuche ich es neu. Problem: der Input wird nicht gefunden, aber Travel für die Varaiabel ausgegeben, vl nochmal strip(), dann mabye

--- bookstoscrape.py
+++ bookstoscrape.py
@@ -5,7 +5,7 @@ import requests
 import re
 
 
-genre = input("Welches Genre möchtest du? ")
+input1 = input("Welches Genre möchtest du? ").lower()
 url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
@@ -15,14 +15,37 @@ doc = BeautifulSoup(page, "html.parser")
 
 genrelist = doc.find(class_="nav nav-list")
 listitems = genrelist.find("li") #Übergeordnetes list
-firstlistelement = listitems.find("li") #Untergeordnetes list
-listelementtext = firstlistelement.text.strip() #Nur der Text der list 
+listelements = listitems.find_all("li") #Untergeordnete lists, hierbei autom. in eine Listenähnlichen Struktur
 
 
-a_tag = firstlistelement.find("a")
-finallink = a_tag['href']
+# listelementtext = listelements[0].text.strip().lower()
+# print(listelementtext)
+# if input1 == listelementtext:
+#     print("Nice das Format von input1 und listelementtext ist gleich")
+
+
+for listelement in listelements:
+    textfound = listelement.find(string=re.compile(input1, re.IGNORECASE)) #hier bin ich stehen geblieben, leider
+    a_tag = listelement.find("a")
+    if textfound == True:
+        break
+    # find_a_tag = textfound.parent
+    # foundlink = find_a_tag['href'] #Angezeigt wird nur der relative Link der mit der Basisurl kombiniert werden muss, checkste?
+
+    print(textfound)
+
+# listelementtext = listelements[1].text #Nur der Text der list 
+# print(listelementtext)
+
+
+
+     
+     
+
+
+
   
-print(finallink)
+
 
 
 
