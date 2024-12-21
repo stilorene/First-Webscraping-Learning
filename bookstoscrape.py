08176ff2e19703ea203854739222a8d33578405e
Rene Marchner <rene.marchner80@gmail.com>
2024-12-21 19:36:57 +0100

    puhh war anstrengen, kann mir aber jetzt alle Buchtitel angeben lassen, werde später noch etwaig damit arbeiten, vl eine Rankingliste basteln, und dem Kunden die Wahl geben zu welchem Buch er gerne den Link haben wollen würde. Zeit - 2h

--- bookstoscrape.py
+++ bookstoscrape.py
@@ -5,41 +5,55 @@ import requests
 import re
 
 
+
 input1 = input("Welches Genre möchtest du? ").lower()
-url = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+url = "https://books.toscrape.com/"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
-# with open("website-content.html", "w", encoding="utf-8") as file:
-#     file.write(page)
 
 genrelist = doc.find(class_="nav nav-list")
 listitems = genrelist.find("li") #Übergeordnetes list
 listelements = listitems.find_all("li") #Untergeordnete lists, hierbei autom. in eine Listenähnlichen Struktur
 
+def getlinkgenre(listelements, url, input1):
+    for listelement in listelements:
+        textfound = listelement.find(string=re.compile(input1, re.IGNORECASE)) #re.IGNORECASE ignoriert Groß- und Kleinschreibung
+        
+        if textfound: #obs halt True ist, ja dann mach das
+            a_tag = listelement.find("a")
+            if a_tag:
+                foundlink = a_tag['href'] #Angezeigt wird nur der relative Link der mit der Basisurl kombiniert werden muss, checkste?
+                combinedlink = url + foundlink
+                # webbrowser.open(foundlink) #Öffnet den Link im Browser
+                 
+                break
 
-#
-# print(listelementtext)
-# if input1 == listelementtext:
-#     print("Nice das Format von input1 und listelementtext ist gleich")
+            print(f"Gefundener Text: {textfound.strip()}")
+    return combinedlink 
+                
+
+# Funktionen um Seiteninhalt und deren Bücher zu scrapen Anfang
+combinedlink = getlinkgenre(listelements, url, input1)
+if combinedlink:
+    print(f"Gefundener kombinierter Link: {combinedlink}")
+else:
+    print("Kein Link gefunden")
 
 
-for listelement in listelements:
-    textfound = listelement.find(string=re.compile(input1, re.IGNORECASE)) #hier bin ich stehen geblieben, leider
-     # Prüfe, ob textfound nicht None ist, bevor du weiter damit arbeitest
-    if textfound:
-        a_tag = listelement.find("a")
-        if a_tag:
-            foundlink = a_tag['href']
-            print(f"Gefundener Link: {foundlink}")
-            break
-    # find_a_tag = textfound.parent
-    # foundlink = find_a_tag['href'] #Angezeigt wird nur der relative Link der mit der Basisurl kombiniert werden muss, checkste?
+genrepage = requests.get(combinedlink).text
+genredoc = BeautifulSoup(genrepage, "html.parser")
 
-        print(f"Gefundener Text: {textfound.strip()}")
+bookinfo = genredoc.find("ol", class_="row") #suche nach den einzelnen Buchelementen
+booklist = bookinfo.find_all("li")
 
-# listelementtext = listelements[1].text #Nur der Text der list 
-# print(listelementtext)
+for element in booklist:
+    booktitle = element.find("h3").text
+    #Suche nach title
+    print(booktitle)
+
+# booktitle = bookinfo.find("li") #.find("h3").text
+
 
 
 
